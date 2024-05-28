accf3a29cbf812cd5e6aa2dd8825a00f00667e34
Fabrice Eklou <fab.eklou@gmail.com>
2024-05-28 10:17:37 +0000

    feat: create and return Pascal's triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""This module contains the  of the function pascal_triangle"""
+
+
+def pascal_triangle(n):
+    """pascal_traiangle create and returns a list of lists
+    of integers representing the Pascal's triangle
+    
+    Arguments:
+        n (int): the height of the triangle
+    
+    Returns:
+        (List[List]): an empty list if n <= 0, a 2D list otherwise
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1] * size for size in range(1, n + 1)]
+    for idx1 in range(2, n):
+        for idx2 in range(1, len(triangle[idx1 - 1])):
+            triangle[idx1][idx2] = triangle[idx1 - 1][idx2 - 1] + triangle[idx1 - 1][idx2]
+  
+    return triangle
