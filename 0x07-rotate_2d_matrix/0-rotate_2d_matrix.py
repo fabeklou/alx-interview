ecd2eaab45e1912ef74a1ef6a9d5d99799eb2c57
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-07-16 12:08:59 +0000

    fix: rm function type-annotation

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -5,10 +5,8 @@ This module contains a function to rotate a 2D matrix in-place
 by 90 degrees clockwise.
 """
 
-from typing import List
 
-
-def rotate_2d_matrix(matrix: List[List[int]]) -> None:
+def rotate_2d_matrix(matrix):
     """
     Rotates a 2D matrix in-place by 90 degrees clockwise.
 
