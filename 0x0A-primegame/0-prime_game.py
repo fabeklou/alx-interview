7a57cf9f22024b96b23172a214cb81a3fda9a547
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-08-07 21:27:27 +0000

    fix: change simulation logic

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -11,80 +11,54 @@ The player who cannot make a move loses the game.
 """
 
 
-def sieve(max_n):
-    """
-    Sieve of Eratosthenes algorithm to generate a list of prime
-    numbers up to a given maximum number.
+def find_primes(num):
+    "find prime numbers up to num"
+    if (type(num) is not int or num < 0):
+        return None
 
-    Args:
-        max_n (int): The maximum number up to which to generate
-            the list of prime numbers.
-
-    Returns:
-        list: A list of prime numbers up to the given maximum number.
-    """
-    is_prime = [True] * (max_n + 1)
-    p = 2
-    while (p * p <= max_n):
-        if is_prime[p]:
-            for i in range(p * p, max_n + 1, p):
-                is_prime[i] = False
-        p += 1
-    prime_list = [p for p in range(2, max_n + 1) if is_prime[p]]
-    return prime_list
+    primes = []
+    for candidate in range(2, num + 1):
+        prime = True
+        for divisor in range(2, candidate):
+            if (candidate % divisor == 0):
+                prime = False
+                break
+        if (prime):
+            primes.append(candidate)
+    return primes
 
 
 def isWinner(x, nums):
-    """
-    Determines the winner of a prime game.
-
-    Args:
-        x (int): The number of rounds to be played.
-        nums (List[int]): A list of integers representing
-            the number of stones in each round.
-
-    Returns:
-        str: The name of the winner ('Maria' or 'Ben')
-            or None if there is no winner.
-    """
-    if not nums or x < 1:
+    "find winner of the prime game"
+    if (type(nums) is not list or not all([type(n) is int for n in nums]) or
+            not all([n > -1 for n in nums])):
         return None
 
-    max_num = max(nums)
-    primes = sieve(max_num)
+    if (type(x) is not int or x != len(nums)):
+        return None
 
-    def play_game(n):
-        remaining = list(range(1, n + 1))
-        player_turn = 0
-        while True:
-            move_made = False
-            for prime in primes:
-                if prime in remaining:
-                    move_made = True
-                    for multiple in range(prime, n + 1, prime):
-                        if multiple in remaining:
-                            remaining.remove(multiple)
-                    break
-            if not move_made:
-                return player_turn
-            player_turn = 1 - player_turn
+    nums.sort()
+    primes = find_primes(nums[-1])
+    if (primes is None):
+        return None
 
-    maria_wins = 0
-    ben_wins = 0
-
+    Maria_wins = 0
+    Ben_wins = 0
     for n in nums:
-        if n == 1:
-            ben_wins += 1
-        else:
-            winner = play_game(n)
-            if winner == 0:
-                maria_wins += 1
+        prime_count = 0
+        for prime in primes:
+            if (prime <= n):
+                prime_count += 1
             else:
-                ben_wins += 1
+                break
+        if prime_count % 2 == 0:
+            Ben_wins += 1
+        else:
+            Maria_wins += 1
 
-    if maria_wins > ben_wins:
-        return 'Maria'
-    elif ben_wins > maria_wins:
-        return 'Ben'
+    if (Maria_wins > Ben_wins):
+        return "Maria"
+    elif (Ben_wins > Maria_wins):
+        return "Ben"
     else:
         return None
--- 0x0A-primegame/main_0.py
+++ 0x0A-primegame/main_0.py
@@ -5,4 +5,4 @@ isWinner = __import__('0-prime_game').isWinner
 print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
 print("Winner: {}".format(isWinner(3, [4, 5, 1])))
 print("Winner: {}".format(isWinner(3, [1, 2, 3])))
-print("Winner: {}".format(isWinner(0, [])))
+print("Winner: {}".format(isWinner(1, [2])))
